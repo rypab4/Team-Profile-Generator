7e77ef1feccb9f44ff6cf56e43fe2bfd3a3575f0
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-01-18 10:59:18 -0800

    added code for intern and manager and connected them to index

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+//this js runs application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 const jest = require('jest');
+
+//add constructors
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Employee');
+const Intern = require('./lib/Intern');
 
